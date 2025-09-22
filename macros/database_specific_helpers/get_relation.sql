df5513cd3e64f0d0e0a6e5d1fed2bf03ece36cea
Codepilot2024 <161473250+Codepilot2024@users.noreply.github.com>
2025-09-22 23:15:06 +0200

    Update get_relation.sql

--- macros/database_specific_helpers/get_relation.sql
+++ macros/database_specific_helpers/get_relation.sql
@@ -1,6 +1,7 @@
 {% macro get_relation(relation_name) %}
     {% if execute %}
         {% set model_get_relation_node = graph.nodes.values() | selectattr('name', 'equalto', relation_name) | first %}
+        {% do log("model_get_relation_node: " ~ model_get_relation_node, info=True) %}
         {% set relation = api.Relation.create(
             database = model_get_relation_node.database,
             schema = model_get_relation_node.schema,
